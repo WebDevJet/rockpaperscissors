527207f851ec72342fa6ae3d87d7a0dfdc156cb2
Josh Tucker <50961547+jettucker@users.noreply.github.com>
2020-01-22 20:28:45 -0600

    added animations and finished logic

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
-const userScore = 0;
-const computerScore = 0;
+let userScore = 0;
+let computerScore = 0;
 const userScore_span = document.getElementById("user-score");
 const computerScore_span = document.getElementById("computer-score");
 const scoreBoard_div = document.querySelector(".score-board");
-const result_div = document.querySelector(".result");
+const result_p = document.querySelector(".result > p");
 const rock_div = document.getElementById("r");
 const paper_div = document.getElementById("p");
 const scissors_div = document.getElementById("s");
@@ -14,9 +14,66 @@ function getComputerChoice() {
     return choices[randomNumber];
 }
 
+function convertToWord(letter) {
+    if (letter === "r") return "Rock";
+    if (letter === "p") return "Paper";
+    return "Scissors";
+}
+
+function win(userChoice, computerChoice) {
+  userScore++;
+  userScore_span.innerHTML = userScore;
+  computerScore_span.innerHTML = computerScore;
+  const smallUserWord = "user".fontsize(3).sub();
+  const smallCompWord = "comp".fontsize(3).sub();
+  result_p.innerHTML = `${convertToWord(userChoice)}${smallUserWord} beats ${convertToWord(computerChoice)}${smallCompWord} . You win!`;
+  document.getElementById(userChoice).classList.add('green-glow');
+  setTimeout(function() { document.getElementById(userChoice).classList.remove('green-glow') }, 1000);
+}
+
+
+
+function lose(userChoice, computerChoice) {
+    computerScore++;
+    userScore_span.innerHTML = userScore;
+    computerScore_span.innerHTML = computerScore;
+    const smallUserWord = "user".fontsize(3).sub();
+    const smallCompWord = "comp".fontsize(3).sub();
+    result_p.innerHTML = `${convertToWord(userChoice)}${smallUserWord} loses to ${convertToWord(computerChoice)}${smallCompWord} . You Lost...`;
+    document.getElementById(userChoice).classList.add('red-glow');
+    setTimeout(function() { document.getElementById(userChoice).classList.remove('red-glow') }, 1000);
+  
+}
+
+function draw(userChoice, computerChoice) {
+    const smallUserWord = "user".fontsize(3).sub();
+    const smallCompWord = "comp".fontsize(3).sub();
+    result_p.innerHTML = `${convertToWord(userChoice)}${smallUserWord} equals ${convertToWord(computerChoice)}${smallCompWord} . It's a draw.`;
+    document.getElementById(userChoice).classList.add('gray-glow');
+    setTimeout(function() { document.getElementById(userChoice).classList.remove('gray-glow') }, 1000);
+  
+}
+
 function game(userChoice) {
  const computerChoice = getComputerChoice();
- console.log(computerChoice);
+ switch (userChoice + computerChoice) {
+     case "rs":
+     case "pr":
+     case "sp":
+         win(userChoice, computerChoice);
+         break;
+     case "rp":
+     case "ps":
+     case"sr":
+         lose(userChoice, computerChoice);
+         break;
+     case "rr":
+     case "pp":
+     case"ss":
+         draw(userChoice, computerChoice);
+         break;
+        }
+ 
 }
 
 game("c");
